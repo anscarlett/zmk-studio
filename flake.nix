aefe0dc25ee6429331b172e524735b3c1f61abaa
Adrian Scarlett <anscarlett@gmail.com>
2026-07-16 09:07:02 +0100

    exclude .direnv from git, update flake

--- flake.nix
+++ flake.nix
@@ -1,45 +1,53 @@
 {
-  description = "zmk-studio development environment";
+  description = "zmk-studio devshell";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
+        pkgs = import nixpkgs { inherit system; };
+      in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nodejs_22
-            cargo
+          packages = with pkgs; [
+            nodejs
             rustc
-            clippy
+            cargo
             rustfmt
+            clippy
             pkg-config
             openssl
-          ];
-
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.openssl
-            pkgs.stdenv.cc.cc.lib
+            udev
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+            glib
+            gtk3
+            webkitgtk_4_1
+            libsoup_3
+            cairo
+            pango
+            gdk-pixbuf
+            atk
+            harfbuzz
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.WebKit
+            darwin.apple_sdk.frameworks.Cocoa
+            darwin.apple_sdk.frameworks.AppKit
+            darwin.apple_sdk.frameworks.Security
+            darwin.apple_sdk.frameworks.SystemConfiguration
           ];
 
           shellHook = ''
-            echo "zmk-studio dev shell"
-            echo "  node $(node --version)"
-            echo "  npm  $(npm --version)"
-            echo "  cargo $(cargo --version)"
-            echo ""
-            echo "  npm install          install dependencies"
-            echo "  npm run storybook    start storybook on :6006"
-            echo "  npm run dev          start vite dev server"
-            echo "  npm run lint         run eslint"
-            echo "  npm run tauri        tauri CLI"
+            export PATH=$(echo "$PATH" | tr ':' '\n' | grep -v "$HOME/.cargo/bin" | paste -sd ':')
+
+            export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.udev.dev}/lib/pkgconfig:${pkgs.dbus.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+            echo "devshell loaded"
+            node -v
+            npm -v
           '';
         };
       });
 }
+
